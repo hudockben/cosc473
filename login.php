6974d99bd97ad2372d090301298ecccd3f4d4930
hudockben <43884062+hudockben@users.noreply.github.com>
2020-01-31 14:22:22 -0500

    Add files via upload
    added the fancy google logo

--- login.php
+++ login.php
@@ -8,42 +8,22 @@ if($_GET['error']==1){
 
 ?>
 
-<head>
-<html>
-<header>
-	<link rel="stylesheet" type="text/css" href="landingpage.css">
-  	<div class="container"> 
-  	  <div id="branding">
-  	  	<a href="landingpage.html"><span class="highlight"><img src="./images/logofinal.png"></span></a>
-  	  </div><!--end branding-->
-  	  <nav>
-        <div id="links">
-  	  	<ul>
-  	  	  <li class="current"><a href="index.html">Home</a></li>
-          <li class="current"><a href="contact.php">Contact</a></li>
-          <li class="current"><a href="register.php">Register</a></li>
-          <li class="current"><a href="index.php">Login</a></li>  
-  	  	</ul>
-        </div><!--end links-->
-  	  </nav>
-  	</div><!--end container-->
-  </header>
-<body>
+ <head>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
-</body>
-</html>
+<meta name="google-signin-scope" content="profile email">
+    <meta name="google-signin-client_id" content="YOUR_CLIENT_ID.apps.googleusercontent.com">
+    <script src="https://apis.google.com/js/platform.js" async defer></script>
 
 
 </head>
-    
-  </header>
- 
+
 <body> 	 
 <form method = "post" action="logincheck.php">
  <div class="container"style="margin-top: 100px">
 	<div class="row justify-content-center">
 		<div class"col-md-6 col-offset-3" align="center">
-		 <br><br>
+		 <img src="images\logo.jpg" style="max-width:550; width:550; max-height:250; height:250;"><br><br>
 		 <form>
 		     <label for="psw"><b>Username</b></label>
 			 <input type="text" placeholder="Enter Username" name="uname" required"><br><br>
@@ -52,24 +32,47 @@ if($_GET['error']==1){
 			<input type="password" placeholder="Enter Password" name="psw" required><br><br>
 			
 			<button type="submit" class="btn btn-outline-secondary">Login</button>
-			<button type="button" class="btn btn-outline-secondary">Login With Google</button>
-			<button type="button" class="btn btn-outline-secondary">Login With Facebook</button><br><br>		
+			<!-- <button type="button" class="btn btn-outline-secondary">Login With Google</button>		-->
+			<div class="g-signin2" data-onsuccess="onSignIn" data-theme="dark"></div>
+    			<script>
+      				function onSignIn(googleUser) {
+        			// Useful data for your client-side scripts:
+        			var profile = googleUser.getBasicProfile();
+        			console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+       				console.log('Full Name: ' + profile.getName());
+       			 	console.log('Given Name: ' + profile.getGivenName());
+        			console.log('Family Name: ' + profile.getFamilyName());
+        			console.log("Image URL: " + profile.getImageUrl());
+        			console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
+    </script>
+
+		<button type="button" class="btn btn-outline-secondary">Login With Facebook</button><br><br>		
 	
 		<input type="checkbox" checked="checked" name="remember"> Remember me
 	
    	<div style="background-color:#DCDCDC">
 
-		<span>Forgot <a href="forgot_password.php
-">password? </a></span>
+		<span>Forgot <a href="forgot_password.php">password? </a></span>
 		</div>
 		</form>
 		 
 		 </div>
 		</div>
 </div>
+
+
+
+
 </form>
+
 <footer>
   	<p>PSH Web Design, Don't Copy Shit &copy; 2020</p>
   </footer>
+  
 </body>
  
